f68aac1f0600f997e1e8fd0600b99563feaecf1d
Percide07 <percidehonhonou@gmail.com>
2023-12-12 14:38:09 +0100

    adding new modification for get and post request for receiverId, create a new message and conversations

--- app.js
+++ app.js
@@ -110,12 +110,11 @@ app.get('/api/conversations/:userId', async (req,res)=>{
     try {
         const userId = req.params.userId;
         const conversations = await Conversations.find({members : {$in: [userId] } } );
-        const conversationUserData = Promise.all(conversations.map(async(conversation)=> {
-            const receiverId = conversation.members.find((member)=>member !== userId);
+        const conversationUserData = Promise.all(conversations.map(async (conversation)=> {
+            const receiverId = conversation.members.find((member)=> member !== userId);
             const user = await Users.findById(receiverId);
-            return { user : {email:user.email, fullName: user.fullName}, conversationId: conversation._id};
+            return { user : {receiverId: user._id, email:user.email, fullName: user.fullName}, conversationId: conversation._id};
         }) )
-        console.log('conversationUserData :: =>', await conversationUserData)
         res.status(200).json( await conversationUserData);
     } catch (error) {
         console.log(error,"Error");
@@ -125,15 +124,15 @@ app.get('/api/conversations/:userId', async (req,res)=>{
 
 app.post('/api/message', async (req,res)=>{
     try {
-        const { conversationId , senderId , message } = req.body;
+        const { conversationId , senderId , message , receiverId = '' } = req.body;
         if (!senderId || !message) return res.status(400).send('Please fill all required fields');
-        if (!conversationId && receiverId) {
-            const newConversation = new Conversations({members : [senderId,receiverId] });
+        if (!conversationId === 'new' && receiverId) {
+            const newConversation = new Conversations({members : [senderId, receiverId] });
             await newConversation.save();
             const newMessage = new Messages({conversationId: newConversation._id, senderId, message});
             await newMessage.save();
             return res.status(200).send('Message sent successfully');
-        }else{
+        }else if(!conversationId && !receiverId) {
             return res.status(400).send('Please fill all required fields');
         }
         const newMessage = new Messages({ conversationId, senderId, message});
@@ -145,14 +144,14 @@ app.post('/api/message', async (req,res)=>{
 })
 
 
-app.get('/api/message/:conversationId', async(req,res)=>{
+app.get('/api/message/:conversationId', async(req, res)=>{
     try {
         const conversationId = req.params.conversationId;
-        if (!conversationId) return res.status(200).json([]);
-        const messages = await Messages.find({conversationId})
+        if (conversationId === 'new') return res.status(200).json([]);
+        const messages = await Messages.find({ conversationId })
         const messageUserData = Promise.all(messages.map(async (message)=>{
             const user = await Users.findById(message.senderId);
-            return { user: { email:user.email, fullName: user.fullName }, message : message.message }
+            return { user: { id: user._id, email: user.email, fullName: user.fullName }, message: message.message }
         } ));
         res.status(200).json(await messageUserData);
     } catch (error) {
