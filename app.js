82efd17357fe67ac23b5350ce663617f8da28854
Percide07 <percidehonhonou@gmail.com>
2023-12-14 10:08:31 +0100

    adding last modifications for back-end

--- app.js
+++ app.js
@@ -48,8 +48,17 @@ io.on('connection', socket =>{
         const receiver = users.find( user => user.userId === receiverId);
         const sender = users.find(user=> user.userId === senderId);
         const user = await Users.findById(senderId);
+        console.log('sender=>', sender,receiver);
         if (receiver) {
             io.to(receiver.socketId).to(sender.socketId).emit('getMessage',{
+                senderId,
+                message,
+                conversationId,
+                receiverId,
+                user: { id: user._id, fullName: user.fullName, email: user.email }
+            });
+        } else {
+            io.to(sender.socketId).emit('getMessage',{
                 senderId,
                 message,
                 conversationId,
